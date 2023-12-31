f4ecfb7413611c3779ca599a2134205501bf3dd6
Neal-C <pop6d531@duck.com>
2023-12-31 16:54:02 +0100

    feat(COMPILER):(red): PAGE 40 - added Compiler Tests && testing helper Fns

---
+++ compiler/compiler_test.go
@@ -0,0 +1,127 @@
+package compiler
+
+import (
+	"fmt"
+	"github.com/Neal-C/compiler-in-go/ast"
+	"github.com/Neal-C/compiler-in-go/code"
+	"github.com/Neal-C/compiler-in-go/lexer"
+	"github.com/Neal-C/compiler-in-go/object"
+	"github.com/Neal-C/compiler-in-go/parser"
+	"testing"
+)
+
+type CompilerTestCase struct {
+	input                string
+	expectedConstants    []any
+	expectedInstructions []code.Instructions
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tableTests := []CompilerTestCase{
+		{
+			input:                "1 + 2",
+			expectedConstants:    []any{1, 2},
+			expectedInstructions: []code.Instructions{code.Make(code.OpConstant, 0), code.Make(code.OpConstant, 1)},
+		},
+	}
+
+	runCompilerTests(t, tableTests)
+}
+
+func runCompilerTests(t *testing.T, tests []CompilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		myCompiler := New()
+
+		err := myCompiler.Compile(program)
+
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := myCompiler.ByteCode()
+
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+
+		err = testConstants(tt.expectedConstants, bytecode.Constants)
+
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	myLexer := lexer.New(input)
+	myParser := parser.New(myLexer)
+	program := myParser.ParseProgram()
+
+	return program
+}
+
+func testInstructions(expectedInstructions []code.Instructions, actualInstructions code.Instructions) error {
+	concatted := concatInstructions(expectedInstructions)
+
+	if len(actualInstructions) != len(concatted) {
+		return fmt.Errorf("wrong instructions length. \n want = %q \n got = %q", concatted, actualInstructions)
+	}
+
+	for index, instruction := range concatted {
+		if actualInstructions[index] != instruction {
+			return fmt.Errorf("wrong instruction at position %d.\nwant = %q\n got= %q", index, concatted, actualInstructions[index])
+		}
+	}
+
+	return nil
+}
+
+func concatInstructions(instructions []code.Instructions) code.Instructions {
+	var out code.Instructions
+
+	for _, instruction := range instructions {
+		out = append(out, instruction...)
+	}
+
+	return out
+}
+
+func testConstants(expected []any, actual []object.Object) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. got = %d , want = %d", len(actual), len(expected))
+	}
+
+	for index, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[index])
+
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed : %s ", index, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+
+	if !ok {
+		return fmt.Errorf("object is not *object.Integer. got = %T (%v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value, got = %d , want = %d", result.Value, expected)
+	}
+
+	return nil
+
+}
