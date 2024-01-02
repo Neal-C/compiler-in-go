a8d74ebd58c8cd1c697ad7bb7555ef775c4e94ce
Neal-C <pop6d531@duck.com>
2024-01-02 15:50:18 +0100

    feat(COMPILER): PAGE 46 - impl'd Compiler struct Fns

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -18,7 +18,38 @@ func New() *Compiler {
 	}
 }
 
-func (self *Compiler) Compile(code ast.Node) error {
+func (self *Compiler) Compile(node ast.Node) error {
+
+	switch node := node.(type) {
+	case *ast.Program:
+
+		for _, stmt := range node.Statements {
+			err := self.Compile(stmt)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := self.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := self.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = self.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		self.emit(code.OpConstant, self.addConstants(integer))
+	}
+
 	return nil
 }
 
@@ -27,6 +58,27 @@ func (self *Compiler) ByteCode() *ByteCode {
 		Instructions: self.instructions,
 		Constants:    self.constants,
 	}
+}
+
+func (self *Compiler) addConstants(obj object.Object) int {
+	self.constants = append(self.constants, obj)
+
+	return len(self.constants) - 1
+}
+
+func (self *Compiler) addInstruction(instructions code.Instructions) int {
+	posNewInstruction := len(self.instructions)
+	self.instructions = append(self.instructions, instructions...)
+
+	return posNewInstruction
+}
+
+func (self *Compiler) emit(op code.Opcode, operands ...int) int {
+
+	instruction := code.Make(op, operands...)
+	position := self.addInstruction(instruction)
+
+	return position
 }
 
 type ByteCode struct {
