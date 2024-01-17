ce28c404fd603ce15503f1fc280d211e09335c5f
Neal-C <pop6d531@duck.com>
2024-01-17 18:18:37 +0100

    feat(COMPILER::symbols):(red): PAGE 120 - added Symbol table tests

--- compiler/symbol_table_test.go
+++ compiler/symbol_table_test.go
@@ -8,17 +8,42 @@ func TestDefine(t *testing.T) {
 		"b": Symbol{Name: "b", Scope: GlobalScope, Index: 1},
 	}
 
-	gloabal := NewSymbolTable()
+	global := NewSymbolTable()
 
-	a := gloabal.Define("a")
+	a := global.Define("a")
 
 	if a != expected["a"] {
 		t.Errorf("expected a=%+v , got %+v", expected["a"], a)
 	}
 
-	b := gloabal.Define("b")
+	b := global.Define("b")
 
 	if b != expected["b"] {
 		t.Errorf("expected b=%+v , got %+v", expected["b"], b)
 	}
+}
+
+func TestResolveGlobal(t *testing.T) {
+	global := NewSymbolTable()
+	global.Define("a")
+	global.Define("b")
+
+	expected := []Symbol{
+		Symbol{Name: "a", Scope: GlobalScope, Index: 0},
+		Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+	}
+
+	for _, symbol := range expected {
+		result, ok := global.Resolve(symbol.Name)
+
+		if !ok {
+			t.Errorf("name %s is not resolvable", symbol.Name)
+			continue
+		}
+
+		if result != symbol {
+			t.Errorf("expected %s to resolve to %+v , got %+v", symbol.Name, symbol, result)
+		}
+	}
+
 }
