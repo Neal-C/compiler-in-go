02cdd2f62a8fdce178d7d71e2fc10209c5f08075
Neal-C <pop6d531@duck.com>
2024-01-19 16:21:49 +0100

    feat(VM):(red): added String tests

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -84,6 +84,13 @@ func testExpectedObject(t *testing.T, expected any, actual object.Object) {
 		if err != nil {
 			t.Errorf("testBooleanObject failed: %s", err)
 		}
+
+	case string:
+		err := testStringObject(expected, actual)
+
+		if err != nil {
+			t.Errorf("testStringObject failed : %s", err)
+		}
 	case *object.Null:
 		if actual != Null {
 			t.Errorf("object is not Null: %T (%v)", actual, actual)
@@ -187,4 +194,28 @@ func TestGlobalLetStatements(t *testing.T) {
 	}
 
 	runVmTests(t, testTable)
+}
+
+func TestStringExpressions(t *testing.T) {
+	testTable := []VmTestCase{
+		{`"monkey"`, "monkey"},
+		{`"mon" + "key"`, "monkey"},
+		{`"mon" + "key" + "banana"`, "monkeybanana"},
+	}
+
+	runVmTests(t, testTable)
+}
+
+func testStringObject(expected string, actual object.Object) error {
+	result, ok := actual.(*object.String)
+
+	if !ok {
+		return fmt.Errorf("object is not String. got = %T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got = %q , want = %q", result.Value, expected)
+	}
+
+	return nil
 }
