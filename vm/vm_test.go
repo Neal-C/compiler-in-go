4f5866cc0848f9982939a9c79af18cb7e18edecd
Neal-C <pop6d531@duck.com>
2024-01-21 07:11:06 +0100

    feat(VM): extended TestExpectObject

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -116,6 +116,33 @@ func testExpectedObject(t *testing.T, expected any, actual object.Object) {
 				t.Errorf("testIntegerObject failed : %s", err)
 			}
 		}
+
+	case map[object.HashKey]int64:
+		hash, ok := actual.(*object.Hash)
+
+		if !ok {
+			t.Errorf("object is not a Hash. got = %T (%+v)", actual, actual)
+			return
+		}
+
+		if len(hash.Pairs) != len(expected) {
+			t.Errorf("hash has wrong number of pairs. want = %d , got = %d", len(expected), len(hash.Pairs))
+			return
+		}
+
+		for expectedKey, expectedValue := range expected {
+			pair, ok := hash.Pairs[expectedKey]
+
+			if !ok {
+				t.Errorf("no pair for given key in Pairs")
+			}
+
+			err := testIntegerObject(expectedValue, pair.Value)
+
+			if err != nil {
+				t.Errorf("testIntegerObject failed : %s", err)
+			}
+		}
 	}
 
 }
