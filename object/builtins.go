6ae79a30fc01061844c7d1810b2c66a229f38b43
Neal-C <pop6d531@duck.com>
2024-02-04 17:01:59 +0100

    feat(OBJECT): moving Builtins for refactoring

---
+++ object/builtins.go
@@ -0,0 +1,35 @@
+package object
+
+import (
+	"fmt"
+)
+
+var Builtins = []struct {
+	Name    string
+	Builtin *Builtin
+}{
+	{
+		Name: "len",
+		Builtin: &Builtin{
+			Fn: func(args ...Object) Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1", len(args))
+				}
+
+				switch arg := args[0].(type) {
+				case *String:
+					return &Integer{Value: int64(len(arg.Value))}
+				case *Array:
+					return &Integer{Value: int64(len(arg.Elements))}
+				default:
+					return newError("argument to len not supported, got %s", args[0].Type())
+				}
+
+			},
+		},
+	},
+}
+
+func newError(format string, a ...any) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
